33d1b1d1d39b700ca4141edb8c7f764281bdd76b
HimanshuJ-dev <himanshu.rkj3@gmail.com>
2023-11-30 12:56:01 +0530

    added initial files with the initial required dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+
+//google fonts
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+//google fonts end
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
